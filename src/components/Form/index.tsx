4917a8d58bab140dfb6d56ad7660d7637e51de15
jonassantospuah <jonas.santos@puah.com.br>
2023-11-13 15:10:59 -0300

    feat: components

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../Button";
+
+export default class Form extends React.Component{
+    render() {
+        return (
+            <form>
+                <div>
+
+                </div>
+                <div>
+                    <Button />
+                </div>
+            </form>
+        )
+    }
+}
